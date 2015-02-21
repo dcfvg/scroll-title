bec3d9a03239e210276b6ef01cfb9d14d30a127a
dcfvg <qwsx@dcfvg.com>
2015-02-21 20:06:33 +0100

    v1

---
+++ assets/js/st.js
@@ -0,0 +1,49 @@
+
+
+$(document).ready(function($) {
+	
+	function setAnim(){
+
+		var $container = $(this),
+				way = ($container.parent().hasClass("bottom")) ? 1 : -1,
+				show = TweenMax.fromTo($(this),1, {"opacity":0}, {"opacity":1});
+				scene = new ScrollScene({triggerElement:$container, duration: 5, triggerHook:"onEnter"})
+		
+		scene.setTween(show).addTo(controller)
+		//scene.addIndicators();
+
+		$container.find('span').each(function() {
+		
+			var $letter = $(this),
+					duration = rand((wHeight/4),(wHeight * .8)),
+	
+					tween = TweenMax.fromTo($letter, duration, 
+						{"top": (wHeight + 200 ) * way +"px"},
+						{"top":0}
+					),
+					scene = new ScrollScene({triggerElement: $container, duration: duration, triggerHook:"onEnter"})
+			
+			$letter.addClass("animated");
+			scene.setTween(tween).addTo(controller)
+			//scene.addIndicators();
+
+		})
+	}
+
+	function rand(min,max){ return Math.floor(Math.random()*(max-min+1)+min); }; 
+
+	var controller, wHeight = $(window).height();
+
+	controller = new ScrollMagic();
+
+	console.log(wHeight);
+
+
+	$('#messages h1 em, #messages h2 em').lettering('words').each(setAnim);
+	$('#messages h1 strong, #messages h2 strong').lettering().each(setAnim);
+	$('#messages p em').lettering('lines').each(setAnim);
+	$('#messages p strong').lettering('words').each(setAnim);
+
+
+
+})
--- index.php
+++ index.php
@@ -6,64 +6,25 @@ include("lib/parsedownExtra.php");
 $Extra = new ParsedownExtra();
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8"> 
-	<title></title>
-	
-	<link href='http://fonts.googleapis.com/css?family=Luckiest+Guy' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="css/screen.css" type="text/css">	
+	<link rel="stylesheet" href="assets/css/screen.css" type="text/css">	
 </head>
-<body class="container-fluid">
-
-	<?php if(isset($_POST['message'])): ?>
-		<form action="" method="post" class="form-horizontal">
-			<fieldset>
-				<legend>scroll-title</legend>
-				<textarea id="message" class="col-sm-6" name="message">artistic *lab*</textarea>
-				<button id="singlebutton" name="singlebutton" class="btn btn-primary col-sm-6">Button</button>
-			</fieldset>
-		</form>
-	<?php else:?>
-
+<body class="container">
 	<h4>scroll down !</h4>
-<hr>
-		<h1 class="message">ARTISTIC lab</h1>
-		<h1 class="message">Contre - Courant Tokyo</h1>
-		<h1 class="message">CCMMP CCMMP CCMMP CCMMP CCMMP CCMMP </h1>
-	<?php endif ?>
 
+	<div id="messages">
+		<?php foreach (glob("content/*.txt") as $f) echo $Extra->text(file_get_contents ($f));?>
+	</div>
 
-
-	<script type="text/javascript" src="js/greensock/TweenMax.min.js"></script>
-	<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
-  <script type="text/javascript" src="js/jquery.lettering-0.6.1.min.js"></script>
-	<script type="text/javascript" src="js/jquery.superscrollorama.js"></script>
-	<script>
-		$(document).ready(function() {
-			var controller = $.superscrollorama();
-			$('.message').lettering().each(function() {
-
-				var parent = $( this );
-				controller.addTween(parent, TweenMax.to(parent, 0.01, {css:{opacity: 1}}));
-
-				$( this ).find('span').each(function() {
-					controller.addTween(
-							parent,
-							TweenMax.from( $( this ), 300, 
-								{css:{top:+rand(-1500,-1000)+'px'}}
-							),
-							300
-						);
-				})
-			});
-			function rand(min,max)
-			{
-			    return Math.floor(Math.random()*(max-min+1)+min);
-			}
-		});
-	</script>
+	<script type="text/javascript" src="assets/js/jquery.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.lettering-0.6.1.min.js"></script>
+	<script type="text/javascript" src="assets/js/TweenMax.min.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.scrollmagic.min.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.scrollmagic.debug.js"></script>
+	<script type="text/javascript" src="assets/js/st.js"></script>
 </body>
 </html>
+
